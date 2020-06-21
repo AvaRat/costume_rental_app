769064b0604dfa0df919ae4171bd6149c10157af
Marcel <marcelos.kalinski@gmail.com>
2020-06-21 19:23:45 +0200

    add cors origin

--- rental_app/main.py
+++ rental_app/main.py
@@ -20,6 +20,7 @@ security = HTTPBasic()
 origins = [
     'https://wypozyczalniastrojowteatrlnych.herokuapp.com',
     "http://localhost:8080",
+    "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
